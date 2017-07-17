f6b0a3aa6fba64892e2aa03834fad4c288e3e8d6
SMTDante <smtdante@gmail.com>
2017-07-17 11:22:03 -0400

    test for lottery

--- winningnumbers.rb
+++ winningnumbers.rb
@@ -1,10 +1,39 @@
-def winner(my_num, win_num)
-
+def lottery(my_num, win_num1)
 	match = []
-	win_num.each_with_index do |value, index_pos|
-		if my_num == value
+	win_num1.each do |match_num|
+		if match_num == my_num
 			match << my_num
 		end
 	end
-	p match
+	match
+end
+
+def close_no_cigar(my_num, win_num1)
+	count = 0
+	z = 0
+	win_num1.length.times do
+		i = 0
+		win_num1.length.times do
+		if my_num[z] == win_num1[i]
+			count += 1
+			end
+			i += 1
+		end
+		z += 1
+	end
+		if count == win_num1.length
+			count
+		elsif count == win_num1.length - 1
+			count
+		end
+	end
+
+def winning_sets(my_num, win_num1)
+	winning_set = []
+	win_num1.each do |x|
+		if close_no_cigar(my_num, x)
+			winning_set << x
+		end
+	end
+	winning_set
 end
